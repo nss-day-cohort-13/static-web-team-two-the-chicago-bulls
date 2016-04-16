0ed0f7d059074f4c65f153da423ba85561f3e14e
Simon Herrera <simonherrera.tech@gmail.com>
2016-04-15 19:09:24 -0500

    Fix by adding 2nd js file for nav

---
+++ scriptNav.js
@@ -0,0 +1,13 @@
+
+var htmlLinks = document.getElementById('main-header');
+htmlLinks.innerHTML =
+	"<nav><ul><li><a href='index.html'>Vacay All Day</a></li>" +
+	"<li><a href='employees.html'>Employees</a></li>" +
+	"<li><a href='history.html'>History</a></li>" +
+	"<li><a href='mission.html'>Mission</a></li>" +
+	"<li><a href='contact.html'>Contact</a></li></ul></nav>";
+
+// console.log("textlinks", htmlLinks );
+
+
+
